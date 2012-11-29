c5570e583b1ac827e9f63013b04c51a985ae06cc
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 10:32:23 +0100

    Make Python examples compatible to Protocol V2.0

--- software/examples/python/example_callback.py
+++ software/examples/python/example_callback.py
@@ -8,16 +8,17 @@ UID = "ayQskyoNrCW" # Change to your UID
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.brick_imu import IMU
 
-imu = IMU(UID) # Create device object
 
 # Quaternion callback
 def quaternion_cb(x, y, z, w):
     print("x: " + str(x) + "\ny: " + str(y) + "\nz: " + str(z) + "\nw: " + str(w) + "\n")
 
 if __name__ == "__main__":
-    ipcon = IPConnection(HOST, PORT) # Create IPconnection to brickd
-    ipcon.add_device(imu) # Add device to IP connection
-    # Don't use device before it is added to a connection
+    ipcon = IPConnection() # Create IP connection
+    imu = IMU(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
 
     # Set period for quaternion callback to 1s
     imu.set_quaternion_period(1000)
@@ -26,4 +27,3 @@ if __name__ == "__main__":
     imu.register_callback(imu.CALLBACK_QUATERNION, quaternion_cb)
 
     raw_input('Press key to exit\n') # Use input() in Python 3
-    ipcon.destroy()
