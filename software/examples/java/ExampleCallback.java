d236032efbe5c8a310ea821cf0f96c56dff95e1a
Olaf Lüke <olaf@tinkerforge.com>
2012-11-29 14:47:07 +0100

    Make Java examples Protocol 2.0 compatible

--- software/examples/java/ExampleCallback.java
+++ software/examples/java/ExampleCallback.java
@@ -7,15 +7,13 @@ public class ExampleCallback {
 	private static final String UID = "ayQskyoNrCW"; // Change to your UID
 
 	// Note: To make the example code cleaner we do not handle exceptions. Exceptions you
-	//       might normally want to catch are described in the commnents below
+	//       might normally want to catch are described in the documentation
 	public static void main(String args[]) throws Exception {
-		// Create connection to brickd
-		IPConnection ipcon = new IPConnection(host, port); // Can throw IOException
-		BrickIMU imu = new BrickIMU(UID); // Create device object
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickIMU imu = new BrickIMU(UID, ipcon); // Create device object
 
-		// Add device to IP connection
-		ipcon.addDevice(imu); // Can throw IPConnection.TimeoutException
-		// Don't use device before it is added to a connection
+		ipcon.connect(host, port); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set period for quaternion callback to 1s
 		imu.setQuaternionPeriod(1000);
@@ -28,6 +26,5 @@ public class ExampleCallback {
 		});
 
 		System.console().readLine("Press key to exit\n");
-		ipcon.destroy();
 	}
 }
