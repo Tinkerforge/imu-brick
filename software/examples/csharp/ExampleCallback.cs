ae2916c8f03b2f1f46f59a4e110b90f64b89c558
Olaf Lüke <olaf@tinkerforge.com>
2012-12-05 15:57:35 +0100

    Add protocol 2.0 compatability to C# bindings

--- software/examples/csharp/ExampleCallback.cs
+++ software/examples/csharp/ExampleCallback.cs
@@ -6,29 +6,27 @@ class Example
 	private static int PORT = 4223;
 	private static string UID = "ayQskyoNrCW"; // Change to your UID
 
-	private static BrickIMU imu;
-
 	// Quaternion callback
-	static void QuaternionCB(float x, float y, float z, float w)
+	static void QuaternionCB(object sender, float x, float y, float z, float w)
 	{
 		System.Console.WriteLine("x: " + x + "\ny: " + y + "\nz: " + z + "\nw: " + w + "\n");
 	}
 
 	static void Main() 
 	{
-		IPConnection ipcon = new IPConnection(HOST, PORT); // Create connection to brickd
-		imu = new BrickIMU(UID); // Create device object
-		ipcon.AddDevice(imu); // Add device to IP connection
-		// Don't use device before it is added to a connection
+		IPConnection ipcon = new IPConnection(); // Create IP connection
+		BrickIMU imu = new BrickIMU(UID, ipcon); // Create device object
+
+		ipcon.Connect(HOST, PORT); // Connect to brickd
+		// Don't use device before ipcon is connected
 
 		// Set period for quaternion callback to 1s
 		imu.SetQuaternionPeriod(1000);
 
 		// Register quaternion callback to QuaternionCB
-		imu.RegisterCallback(new BrickIMU.Quaternion(QuaternionCB));
+		imu.Quaternion += QuaternionCB;
 
 		System.Console.WriteLine("Press key to exit");
 		System.Console.ReadKey();
-		ipcon.Destroy();
 	}
 }
