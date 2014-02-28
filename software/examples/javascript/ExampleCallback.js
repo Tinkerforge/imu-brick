3872431340e240708ce09a2e8defb25f8c1aed86
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:16:26 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -2,18 +2,18 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '6QFQff';// Change to your UID
+var UID = '6QFQff'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var imu = new Tinkerforge.BrickIMU(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var imu = new Tinkerforge.BrickIMU(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set period for quaternion callback to 1s
@@ -40,4 +40,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
