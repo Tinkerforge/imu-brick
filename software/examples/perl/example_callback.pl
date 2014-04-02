ca05f2a53381fe14a92570c215a3c36b356f28a1
Matthias Bolte <matthias@tinkerforge.com>
2014-04-02 19:28:03 +0200

    perl: Unify print style

--- software/examples/perl/example_callback.pl
+++ software/examples/perl/example_callback.pl
@@ -10,11 +10,12 @@ use constant UID => '6R4m4d'; # Change to your UID
 # Quaternion callback
 sub cb_quaternion
 {
-	my ($x, $y, $z, $w) = @_;
-	print "\nx:  ".$x;
-	print "\ny:  ".$y;
-	print "\nz:  ".$z;
-	print "\nw:  ".$w."\n";
+    my ($x, $y, $z, $w) = @_;
+
+    print "x: $x\n";
+    print "y: $y\n";
+    print "z: $z\n";
+    print "w: $w\n";
 }
 
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
@@ -29,6 +30,6 @@ $imu->set_quaternion_period(1000);
 # Register quaternion callback
 $imu->register_callback($imu->CALLBACK_QUATERNION, 'cb_quaternion');
 
-print "\nPress any key to exit...\n";
+print "Press any key to exit...\n";
 <STDIN>;
 $ipcon->disconnect();
