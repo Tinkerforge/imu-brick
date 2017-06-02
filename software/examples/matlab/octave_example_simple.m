1cb850f7403ba493f0706f03fc1cf25e8a2ad5c1
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 15:51:13 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_simple.m
+++ software/examples/matlab/octave_example_simple.m
@@ -5,8 +5,8 @@ function octave_example_simple()
     PORT = 4223;
     UID = "XXYYZZ"; % Change XXYYZZ to the UID of your IMU Brick
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    imu = java_new("com.tinkerforge.BrickIMU", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    imu = javaObject("com.tinkerforge.BrickIMU", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
