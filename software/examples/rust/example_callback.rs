8ad5f6232b27e97fc80c8e69efdb521759af8929
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:26:05 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for quaternion events.
-    let quaternion_receiver = imu.get_quaternion_receiver();
+    let quaternion_receiver = imu.get_quaternion_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `imu` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `imu` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for quaternion in quaternion_receiver {
